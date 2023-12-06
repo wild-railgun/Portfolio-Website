dd1f7ffc967b35968ff481cc60244b85fa465939
wild-railgun <wildrailgun@gmail.com>
2023-12-06 14:31:20 +0500

    Added Styles to Navbar and Added Some Files

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function toggleMenu() {
+function toggleNavMenu() {
     const menu = document.querySelector(".burger-links");
     const icon = document.querySelector(".burger-icon");
     menu.classList.toggle("open");
