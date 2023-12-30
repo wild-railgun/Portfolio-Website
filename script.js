f4d34e0495d669222753d5ae99fbd1bd06047faa
wild-railgun <wildrailgun@gmail.com>
2023-12-31 00:27:52 +0500

    Some Fixes

--- script.js
+++ script.js
@@ -4,3 +4,10 @@ function toggleNavMenu() {
     menu.classList.toggle("open");
     icon.classList.toggle("open");
 }
+
+function focus() {
+    const menu = document.querySelector(".child1");
+    const icon = document.querySelector(".child2");
+    menu.classList.toggle("clicked");
+    icon.classList.toggle("clicked");
+}
